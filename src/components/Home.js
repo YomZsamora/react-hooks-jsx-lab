4bd2de8e419c241619a76c8a5a717d63c2d82eff
yomZsamora <samora.y@adzumi.co.ke>
2022-03-10 18:33:53 +0300

    commit tree 8de74cf0d9816516ecee1675e0986cc1260e1ece

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
-}
+const headerStyle = {
+   color: 'firebrick',
+};
 
-export default Home;
+function Home() {
+   // update the JSX being returned!
+   return (
+      <div id="home">
+      <h1 style={headerStyle}>{name} is a Web Developer from {city}</h1>
+      </div>
+      );
+   }
+   
+   export default Home;
+   
